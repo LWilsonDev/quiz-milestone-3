1852d56b46c6abe470e0f86578a7864a09d8a7f4
Lucy Wilson <lucyleleu@googlemail.com>
2018-08-28 17:10:36 +0000

    functions added and tested

--- app.py
+++ app.py
@@ -9,10 +9,23 @@ def ask_question(num):
     if num <= len(questions):
         return questions[num]['question']
     return "finished quiz"    
+    
+def get_answer(num):
+    return questions[num]['answer']
+    
+def question_count():
+    q_count = 1
+    return q_count
+    
+def check_answer(answer):
+    #answer = str(request.form['user_answer']).lower()
+    if answer == questions[1]['answer']:
+        return "correct"
+    return "false"    
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    pass
+    return render_template('index.html')
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)  
--- test_app.py
+++ test_app.py
@@ -5,17 +5,36 @@ from app import *
 
 
 class TestQuestions(unittest.TestCase):
-  '''
-  Function that finds the question from the dictionary
-  '''
+ 
   def test_ask_question(self):
+    '''
+    Function that finds the question from the dictionary
+    '''
     self.assertEqual(ask_question(1), "What is the common name for this tree?")
     self.assertEqual(ask_question(2), "Which japanese plant has leaves like this?")
     #if question index is > len(questions), should return "finished quiz"
     self.assertEqual(ask_question(len(questions)+1), "finished quiz")
     
   def test_get_answer(self):
+    '''
+    Function that finds the answer from the dictionary
+    '''  
     self.assertEqual(get_answer(1), "oak")
+    self.assertEqual(get_answer(2), "wasabi")
+    
+  def test_check_answer(self):
+    '''
+    Function that takes user inputed answer and compares it to the right answer
+    '''
+    self.assertEqual(check_answer("oak"), "correct")
+    self.assertNotEqual(check_answer("good"), "correct")
+    self.assertNotEqual(check_answer("oak"), "false")
+    
+class TestQuestionCounter(unittest.TestCase):
+    
+  def test_question_count(self):
+    self.assertEqual(question_count(), 1) 
+    
     
 
 if __name__ == '__main__':
