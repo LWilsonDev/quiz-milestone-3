fe6626ef4290a09fcf3feced8f59c0d260515abf
Lucy Wilson <lucyleleu@googlemail.com>
2018-08-29 17:27:57 +0000

    global function for q_index

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from flask import Flask, redirect, render_template, request, session, url_for, f
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+
+
+
+
+
 def ask_question(num):
     if num <= len(questions):
         return questions[num]['question']
@@ -20,59 +25,52 @@ def check_answer(answer, num):
     
 def init():
     score = 0
-    ask_question(1)
-    q_index= 1
-    context = {
-        'q_count': 1,
-        'question': questions[q_index]['question'],
-        'answer': questions[q_index]['answer'],
-        
-    }
-    return context
+    q_index = 1
+    return q_index
     
+    
+        
+q_index = 1
+question = ask_question(q_index)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
-        
-    #display_question = ask_question(q_count)
-    
-        #answer = str(request.form['user_answer']).lower()
-   
     return render_template('index.html')    
-#        if check_answer(answer, q_count) == True:
-#            flash("correct")
-#            q_count += 1
-#        else:
-#            flash("false")
-#        if request.form['submit_btn'] == 'next':
-#            
-#            return redirect(url_for('index'))    
-#    
-#    return render_template('index.html',
-#    question=display_question)
+
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
+    global q_index
+    global question
+    
     if request.method == "POST":
-        if request.form['start'] == 'start':
-            context = init()
-            return render_template('quiz.html', context=context)
         
-#        
-#    
-#    return render_template('quiz.html', context=context)    
-#        if check_answer(answer, q_count) == True:
-#            flash("correct")
-#            q_count += 1
-#        else:
-#            flash("false")
-#        if request.form['submit_btn'] == 'next':
-#            
-#            return redirect(url_for('index'))    
-#    
-#    return render_template('index.html',
-#    question=display_question)
+        if request.form.get('start') == 'start':
+            #POST request coming from landing page ie. start game
+            
+            return render_template('quiz.html', question=question, q_index=q_index)
+        
+        elif request.form['submit_btn'] == 'submit':
+            #POST request from submit button. Check answer
+            answer = str(request.form['user_answer']).lower()
+            q_index = int(request.form.get('q_index'))
+            if check_answer(answer, q_index) == True:
+                flash('YES!')
+            else:
+                flash('Incorrect') 
+               
+            
+            
+        elif request.form['submit_btn'] == 'next':
+            q_index += 1
+            question = ask_question(q_index)
+        return render_template('quiz.html', question=question, q_index=q_index)
+            
+         
+
+    #if GET request show error and redirect to home page
+    flash('Oops')
+    return redirect('/')
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)  
