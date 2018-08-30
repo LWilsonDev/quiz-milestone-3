50c96c5f575d6c50b2c18dd5f829075de3cca1f5
Lucy Wilson <lucyleleu@googlemail.com>
2018-08-30 17:44:42 +0000

    added results page and game logic for guesses

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ def get_question_data(index):
         questions = json.loads(json_questions.read())
         return questions[index]
 
+def how_many_questions(): 
+    '''
+    Function to find the number of questions in the json file.
+    '''
+    with open('data/questions.json') as json_questions:
+        q_dict = json.loads(json_questions.read())
+        return len(q_dict)
+
 def get_question(index):
     data = get_question_data(index)
     question = data['question']
@@ -37,34 +45,56 @@ def check_answer(user_answer, index):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    #remove session incase same user is returning
+    for key in session.keys():
+        session.pop(key)
     return render_template('index.html')    
 
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     
-   
     if request.method == "POST":
         
         if "q_index" not in session:
         #start of game - set up first question
             session["q_index"] = 0
-            session["correct_count"] = 0
-            session["guess_num"] = 2
+            session["score"] = 0
+            session["guess"] = 0
             question = get_question(session['q_index'])
             image = get_image(session['q_index'])
        
         if request.form.get('submit_btn', None) == "submit":
             user_answer = str(request.form['user_answer']).lower()
             answer = check_answer(user_answer, session['q_index'])
-            if answer == True:
-                flash('Correct')
+            session['guess'] += 1
+            
+            if session['guess'] <= 2:
+                if answer == True:
+                    session["score"] += 1
+                    session['guess'] = 3 #So that the same correct answer can't be entered again
+                    flash('Correct')
+                else:
+                    if session['guess'] == 1:
+                        flash('Incorrect, try again')
+                    elif session['guess'] == 2:
+                        answer = get_answer(session['q_index'])
+                        flash('Incorrect. The answer is ' + answer.capitalize())
             else:
-                flash('Incorrect')
-        
-        if request.form.get('submit_btn', None) == "next":  
-            session["q_index"] += 1
-            return redirect(url_for('quiz'))
+                flash('You are out of guesses! Click Next to continue')
+                
+        if request.form.get('submit_btn', None) == "next":
+            num_qestions = how_many_questions()
+            session['guess'] = 0
+            if session["q_index"] < num_qestions-1:
+                session["q_index"] += 1
+                return redirect(url_for('quiz'))
+            else:
+                # Display different message if user has scored less than half correct)
+                low_score = session["score"] < num_qestions/2
+                    
+               
+                return render_template('result.html', total_questions=num_qestions, user_score=session["score"], low_score=low_score)
             
     q_index = session["q_index"]
     question = get_question(session['q_index'])
